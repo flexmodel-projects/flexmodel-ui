97b73a4e4b06f5649bcb035810c0b6a92c08df81
Cheng Jinbao <cjbi@outlook.com>
2024-08-25 19:15:08 +0800

    First Commit

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -1,57 +1,66 @@
 import * as React from 'react';
-import { Layout, Menu } from 'antd';
+import {Col, Layout, Menu, Row} from 'antd';
 import * as Icons from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import './Sidebar.css';
 
 interface SidebarState {
-    collapsed: boolean;
-    mode: "vertical" | "inline" | "horizontal" | undefined;
+  collapsed: boolean;
+  mode: "vertical" | "inline" | "horizontal" | undefined;
 }
 
 class Sidebar extends React.Component<unknown, SidebarState> {
-    constructor(props: unknown) {
-        super(props);
-        this.state = {
-            collapsed: false,
-            mode: "inline",
-        };
-    }
+  constructor(props: unknown) {
+    super(props);
+    this.state = {
+      collapsed: false,
+      mode: "inline",
+    };
+  }
 
-    public render() {
-        return (
-            <Layout.Sider collapsible collapsed={this.state.collapsed} onCollapse={this.toggle}>
-                <div className="ant-layout-logo" />
-                <Menu theme="dark" mode={this.state.mode} defaultSelectedKeys={["1"]}>
-                    <Menu.Item key="1">
-                        <Link to="/home">
-                            <Icons.HomeOutlined />
-                            <span className="nav-text">Home</span>
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item key="2">
-                        <Link to="/todo">
-                            <Icons.CheckSquareOutlined />
-                            <span className="nav-text">Todo</span>
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item key="3">
-                        <Link to="/about">
-                            <Icons.FileOutlined />
-                            <span className="nav-text">About</span>
-                        </Link>
-                    </Menu.Item>
-                </Menu>
-            </Layout.Sider>
-        );
-    }
+  public render() {
+    return (
+      <Layout.Sider collapsible collapsed={this.state.collapsed} onCollapse={this.toggle}>
+        <div className="ant-layout-logo">
+          <Row align="middle">
+            <Col>
+              <img src="/logo.svg" width="40px" alt="logo"/>
+            </Col>
+            {this.state.collapsed ? '' : <Col>
+              <span style={{fontSize: "20px", padding: '10px', color: "#eee"}}>Flexmodel</span>
+            </Col>}
+          </Row>
+        </div>
+        <Menu theme="dark" mode={this.state.mode} defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1">
+            <Link to="/home">
+              <Icons.HomeOutlined/>
+              <span className="nav-text">Home</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/todo">
+              <Icons.CheckSquareOutlined/>
+              <span className="nav-text">Todo</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Link to="/about">
+              <Icons.FileOutlined/>
+              <span className="nav-text">About</span>
+            </Link>
+          </Menu.Item>
+        </Menu>
+      </Layout.Sider>
+    );
+  }
 
-    private toggle = () => {
-        this.setState({
-            collapsed: !this.state.collapsed,
-            mode: !this.state.collapsed ? "vertical" : "inline",
-        });
-    }
+  private toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+      mode: !this.state.collapsed ? "vertical" : "inline",
+    });
+  }
 }
 
 export default Sidebar;
