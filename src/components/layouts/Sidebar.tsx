930f8e476c22ef57cad633ca9c8e77e5fcbcc576
Cheng Jinbao <cjbi@outlook.com>
2024-10-09 09:32:55 +0800

    First Commit

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -1,107 +1,96 @@
 import * as React from 'react';
-import {Col, Layout, Menu, Row} from 'antd';
+import { useState } from 'react';
+import { Col, Layout, Menu, Row } from 'antd';
 import * as Icons from '@ant-design/icons';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Sidebar.css';
 import {
   ApiOutlined,
   DatabaseOutlined,
   DeploymentUnitOutlined,
-  FileTextOutlined, LineChartOutlined, SettingOutlined,
+  FileTextOutlined,
+  LineChartOutlined,
+  SettingOutlined,
   UserOutlined
 } from "@ant-design/icons";
-
-interface SidebarState {
-  collapsed: boolean;
-  mode: "vertical" | "inline" | "horizontal" | undefined;
-  selectedKey: string;
-}
 
 interface SidebarProps {
   defaultSelectedKey?: string;
 }
 
-class Sidebar extends React.Component<SidebarProps, SidebarState> {
-  constructor(props: SidebarProps) {
-    super(props);
-    this.state = {
-      collapsed: false,
-      mode: "inline",
-      selectedKey: this.props.defaultSelectedKey || "1"
-    };
-  }
+const Sidebar: React.FC<SidebarProps> = ({ defaultSelectedKey = "1" }) => {
+  const [collapsed, setCollapsed] = useState(false);
+  const [mode, setMode] = useState<"vertical" | "inline" | "horizontal" | undefined>("inline");
 
-  public render() {
-    return (
-      <Layout.Sider collapsible collapsed={this.state.collapsed} onCollapse={this.toggle}>
-        <div className="ant-layout-logo">
-          <Row align="middle">
+  const toggle = () => {
+    setCollapsed(!collapsed);
+    setMode(collapsed ? "inline" : "vertical");
+  };
+
+  return (
+    <Layout.Sider collapsible collapsed={collapsed} onCollapse={toggle}>
+      <div className="ant-layout-logo">
+        <Row align="middle">
+          <Col>
+            <img src="/logo.svg" width="40px" alt="logo" />
+          </Col>
+          {!collapsed && (
             <Col>
-              <img src="/logo.svg" width="40px" alt="logo"/>
+              <span style={{ fontSize: "20px", padding: '10px', color: "#eee" }}>Flexmodel</span>
             </Col>
-            {this.state.collapsed ? '' : <Col>
-              <span style={{fontSize: "20px", padding: '10px', color: "#eee"}}>Flexmodel</span>
-            </Col>}
-          </Row>
-        </div>
-        <Menu theme="dark" mode={this.state.mode} defaultSelectedKeys={[this.state.selectedKey]}>
-          <Menu.SubMenu key="api" icon={<ApiOutlined />} title="API">
-            <Menu.Item key="/api-management">
-              <Link to="/api-management">
-                <DeploymentUnitOutlined />
-                <span className="nav-text">API Management</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="/api-document">
-              <Link to="/api-document">
-                <FileTextOutlined />
-                <span className="nav-text">API Document</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="/api-log">
-              <Link to="/api-log">
-                <LineChartOutlined />
-                <span className="nav-text">API Log</span>
-              </Link>
-            </Menu.Item>
-          </Menu.SubMenu>
-          <Menu.SubMenu key="data" icon={<DatabaseOutlined />} title="Data">
-            <Menu.Item key="/datasource">
-              <Link to="/datasource">
-                <Icons.FileOutlined/>
-                <span className="nav-text">Data source</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="/modeling">
-              <Link to="/modeling">
-                <Icons.FileOutlined/>
-                <span className="nav-text">Data modeling</span>
-              </Link>
-            </Menu.Item>
-          </Menu.SubMenu>
-          <Menu.Item key="/identity-providers">
-            <Link to="/identity-providers">
-              <UserOutlined />
-              <span className="nav-text">Identity Providers</span>
+          )}
+        </Row>
+      </div>
+      <Menu theme="dark" mode={mode} defaultSelectedKeys={[defaultSelectedKey]}>
+        <Menu.SubMenu key="api" icon={<ApiOutlined />} title="API">
+          <Menu.Item key="/api-management">
+            <Link to="/api-management">
+              <DeploymentUnitOutlined />
+              <span className="nav-text">API Management</span>
             </Link>
           </Menu.Item>
-          <Menu.Item key="/settings">
-            <Link to="/settings">
-              <SettingOutlined />
-              <span className="nav-text">Settings</span>
+          <Menu.Item key="/api-document">
+            <Link to="/api-document">
+              <FileTextOutlined />
+              <span className="nav-text">API Document</span>
             </Link>
           </Menu.Item>
-        </Menu>
-      </Layout.Sider>
-    );
-  }
-
-  private toggle = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-      mode: !this.state.collapsed ? "vertical" : "inline",
-    });
-  }
-}
+          <Menu.Item key="/api-log">
+            <Link to="/api-log">
+              <LineChartOutlined />
+              <span className="nav-text">API Log</span>
+            </Link>
+          </Menu.Item>
+        </Menu.SubMenu>
+        <Menu.SubMenu key="data" icon={<DatabaseOutlined />} title="Data">
+          <Menu.Item key="/datasource">
+            <Link to="/datasource">
+              <Icons.FileOutlined />
+              <span className="nav-text">Data source</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="/modeling">
+            <Link to="/modeling">
+              <Icons.FileOutlined />
+              <span className="nav-text">Data modeling</span>
+            </Link>
+          </Menu.Item>
+        </Menu.SubMenu>
+        <Menu.Item key="/identity-providers">
+          <Link to="/identity-providers">
+            <UserOutlined />
+            <span className="nav-text">Identity Providers</span>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="/settings">
+          <Link to="/settings">
+            <SettingOutlined />
+            <span className="nav-text">Settings</span>
+          </Link>
+        </Menu.Item>
+      </Menu>
+    </Layout.Sider>
+  );
+};
 
 export default Sidebar;
