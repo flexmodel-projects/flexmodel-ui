14c9bdc3a426e3c6aa3cb73619acc7e475fc3346
Cheng Jinbao <cjbi@outlook.com>
2024-11-09 19:30:09 +0800

    chore: api doc

--- src/pages/APIDocument/index.tsx
+++ src/pages/APIDocument/index.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 const SwaggerUI: React.FC = () => {
@@ -6,8 +5,8 @@ const SwaggerUI: React.FC = () => {
     <div style={{width: '100%', height: '100%', border: 'none'}}>
       <iframe
         style={{width: '100%', height: '100%', border: 'none'}}
-        src="/swagger-ui/index.html"
-        title="Swagger UI"
+        src="/rapi-doc.html"
+        title="Rapi Doc"
       />
     </div>
 
