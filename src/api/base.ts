8865711404421d16d66b48bdefc95cc02f78b821
Cheng Jinbao <cjbi@outlook.com>
2025-04-11 14:10:08 +0800

    fix: 支持代码生成

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,1 +1,22 @@
 export const BASE_URI = '/fm-api'
+
+export async function getFileAsBlob(url: string) {
+  try {
+    // 发送请求获取文件
+    const response = await fetch(url);
+
+    // 检查请求是否成功
+    if (!response.ok) {
+      throw new Error(`网络响应不正常: ${response.status}`);
+    }
+
+    // 将响应内容转换为 Blob 对象
+    const blob = await response.blob();
+
+    // 返回 Blob 对象
+    return blob;
+  } catch (error) {
+    console.error('获取文件失败:', error);
+    throw error; // 重新抛出错误供上层调用处理
+  }
+}
--- src/pages/DataModeling/components/CodeView.tsx
+++ src/pages/DataModeling/components/CodeView.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import type {Model} from "../data";
-import {Divider, Typography} from 'antd';
+import {Button, Divider, message, Space, Typography} from 'antd';
+import {BASE_URI, getFileAsBlob} from "../../../api/base.ts";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import Explore from "../../../components/explore/explore/Explore.jsx";
 
 const {Paragraph} = Typography;
 
@@ -9,16 +13,56 @@ interface CodeViewProps {
   model?: Partial<Model>;
 }
 
-const CodeView: React.FC<CodeViewProps> = ({model}) => {
+const CodeView: React.FC<CodeViewProps> = ({datasource, model}) => {
+  const [blob, setBlob] = React.useState<Blob | null>(null);
+  const [exploreOpen, setExploreOpen] = React.useState(false);
+
+  // const Explore = lazy(() => import('../../../components/explore/explore/Explore.jsx'))
   return (
     <>
       <div>
         {model?.name} {model?.comment}
       </div>
-      <Divider />
+      <Divider/>
       <pre>
         <Paragraph copyable style={{whiteSpace: "pre-wrap"}}>{model?.idl}</Paragraph>
       </pre>
+      <Space>
+        <Button type="primary" onClick={() => {
+          navigator.clipboard.writeText(model?.idl || '');
+          message.success('Copied');
+        }}>
+          Copy IDL
+        </Button>
+        <Button type="primary" onClick={async () => {
+          const blob = await getFileAsBlob(`${BASE_URI}/codegen/${datasource}_${model?.name}.zip`);
+          setBlob(blob);
+          setExploreOpen(true);
+        }}>
+          Explore
+        </Button>
+
+        <Button type="primary" onClick={async () => {
+          const url: string = `${BASE_URI}/codegen/${datasource}_${model?.name}.zip`;
+          const link = document.createElement('a');
+          link.href = url;
+          link.setAttribute('download', `${datasource}_${model?.name}.zip`); // 替换为你希望保存的文件名
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        }}>
+          Generate code
+        </Button>
+      </Space>
+      <Explore
+        projectName={`${datasource}_${model?.name}.zip`}
+        blob={blob}
+        open={exploreOpen}
+        onClose={() => {
+          setBlob(null);
+          setExploreOpen(false);
+        }}
+      />
     </>
   );
 };
